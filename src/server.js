2813ebcbbc8eb34e6feabc49245ff0a158b2349f
Pablo Basso <pablobasso25@gmail.com>
2026-08-26 20:35:09 -0300

    refactor(instituto): migrate institute endpoints and logic to mvc

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import alumnoRoutes from "./routes/alumno.routes.js";
 import profesorRoutes from "./routes/profesor.routes.js";
 import cursosRoutes from "./routes/curso.routes.js";
+import institutoRoutes from "./routes/instituto.routes.js";
 const app = express();
 
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use("/api/alumnos", alumnoRoutes);
 app.use("/api/profesores", profesorRoutes);
 app.use("/api/cursos", cursosRoutes);
+app.use("/api/instituto", institutoRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
